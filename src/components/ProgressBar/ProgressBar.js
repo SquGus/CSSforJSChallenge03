462d4525174e6041088912db003e62bfbd5dd62e
Lupe Canaviri <lupec@microsoft.com>
2022-06-08 23:00:29 -0700

    feat: Add sizes medium and large

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -4,9 +4,23 @@ import React from 'react';
 import VisuallyHidden from '../VisuallyHidden';
 import styled from 'styled-components';
 
+const SIZES = {
+  small:{
+    height: 8
+  },
+  medium:{
+    height: 12
+  },
+  large:{
+    height: 16    
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
+  const currentSize = SIZES[size];
+  
   return (
-    <WrapperBar role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+    <WrapperBar currentSize = {currentSize} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
       <Progress value={value}></Progress>
     </WrapperBar>
 
@@ -14,15 +28,21 @@ const ProgressBar = ({ value, size }) => {
 };
 
 const WrapperBar = styled.div`
-  width:100%;
-  height:8px;
-  background:${COLORS.transparentGray15};
+  width: 100%;
+  height: ${p => p.currentSize.height}px;
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+  border-radius: 4px;
 `;
 
 const Progress = styled.div`
-  width:${p => p.value}%;
-  height:8px;
-  background:${COLORS.primary};
+  width: ${p => p.value}%;
+  height: 100%;
+  background: ${COLORS.primary};
+  border-top-left-radius: 4px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-bottom-left-radius: 4px;
 `;
 
 export default ProgressBar;

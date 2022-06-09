97be6362d90d1e890df80e8a650ee1085a57df09
Lupe Canaviri <lupec@microsoft.com>
2022-06-08 22:40:24 -0700

    feat: styled default progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,28 @@
+import { COLORS } from '../../constants';
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import VisuallyHidden from '../VisuallyHidden';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
-
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <WrapperBar role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+      <Progress value={value}></Progress>
+    </WrapperBar>
+
+  );
 };
+
+const WrapperBar = styled.div`
+  width:100%;
+  height:8px;
+  background:${COLORS.transparentGray15};
+`;
+
+const Progress = styled.div`
+  width:${p => p.value}%;
+  height:8px;
+  background:${COLORS.primary};
+`;
 
 export default ProgressBar;

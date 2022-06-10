875e3a5b11bd707d1e698f9d086d5f46de0b647e
Lupe Canaviri <lupec@microsoft.com>
2022-06-09 21:31:41 -0700

    feat: Add basic styles for select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
+import React from 'react';
 import { getDisplayedValue } from './Select.helpers';
+import styled from 'styled-components';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <WrapperSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </WrapperSelect>
   );
 };
+
+const WrapperSelect = styled.select`
+/* change:
+- hover
+- focus
+** padding right with the arrow for selector does not move
+*/
+  /*Text styles*/
+  font-weight: 400;
+  font-size: 16px;
+  color: ${COLORS.gray700};
+  /* Select Container */
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  border: none;
+  border-radius: 8px;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+`;
 
 export default Select;

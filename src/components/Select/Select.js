322c2c050ae2716261586baac2b43fc48323e5de
Lupe Canaviri <lupec@microsoft.com>
2022-06-09 23:12:54 -0700

    add the presentation style and hover

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
+import React from 'react';
 import { getDisplayedValue } from './Select.helpers';
+import styled from 'styled-components';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationSelect >
+        hello world
+      </PresentationSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+const PresentationSelect = styled.div`
+/* change:
+- hover
+- focus
+** padding right with the arrow for selector does not move
+*/
+  /*Text styles*/
+  font-weight: 400;
+  font-size: (16 / 16) rem;
+  color: ${COLORS.gray700};
+  /* Select Container */
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  border-radius: 8px;
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+
+`;
 
 export default Select;

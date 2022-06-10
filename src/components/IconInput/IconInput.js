ff7b7495d034ccb96d607cec3180236754663d01
Lupe Canaviri <lupec@microsoft.com>
2022-06-10 01:28:12 -0700

    feat: base for input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,14 @@
+import { COLORS } from '../../constants';
+import Icon from '../Icon';
 import React from 'react';
+import VisuallyHidden from '../VisuallyHidden';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
-
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+const SIZES = {
+  large: {
+    iconSize: 24
+  }
+}
 
 const IconInput = ({
   label,
@@ -13,7 +17,40 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const currSize = SIZES[size]
+  
+  return (
+  <Wrapper>
+    <IconWrapper>
+      <Icon id={icon} size={currSize.iconSize}></Icon>      
+    </IconWrapper>
+    <NativeInput placeholder={placeholder}></NativeInput>
+  </Wrapper>);
 };
+
+const NativeInput = styled.input`
+  border: none;
+  padding-left: 40px;
+  width: 100%;
+  border: none;
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+`;
+
+const Wrapper = styled.div`
+  border-bottom: 2px solid ${COLORS.black};
+  padding-bottom: 8px;
+  color: ${COLORS.gray700};
+  
+ 
+`;
 
 export default IconInput;
